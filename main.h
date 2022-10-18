fb7e7ced6fb9afc9e9644e5cddac1e74d5ac4fd6
Ruffshok <111015150+Ruffshok@users.noreply.github.com>
2022-10-19 00:15:24 +0100

    Update main.h

--- main.h
+++ main.h
@@ -59,28 +59,6 @@ int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 /**
-char *prep_numeric(char *str, specifier spec) prints the number of char in a str
-*returns 0
-*/
-char *prep_numeric(char *str, specifier spec)
-
-/**
-char *prep_string(char *str, specifier spec) prints the number of str in an array
-*returns 0
-*/
-char *prep_string(char *str, specifier spec)
-
-/**
-char* (*get_func(char i))(va_list) prints the char in a list
-*returns 0
-*/
-char* (*get_func(char i))(va_list);
-
-/**
-char *create_buffer(void) prints the buffer
-*returns 0
-*/
-char *create_buffer(void);
 void write_buffer(char *buffer, int len, va_list list);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
